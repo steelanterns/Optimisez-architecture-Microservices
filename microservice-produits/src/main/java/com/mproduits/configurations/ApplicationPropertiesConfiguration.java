b11081daa347bd797ac5f255060a0d7abdb05998
steelanterns <steevejeanchilles@gmail.com>
2022-11-20 16:14:11 -0500

    first commit on this branch

--- microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
+++ microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
@@ -1,12 +1,12 @@
 package com.mproduits.configurations;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("mes-configs")
+@RefreshScope
 public class ApplicationPropertiesConfiguration {
 
     private int limitDeProduits;
